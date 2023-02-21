e2b0030fc2853212d01f386ad369f8b939ddccb5
Noah Vaughn <noahdvaughn@gmail.com>
2023-02-21 16:06:31 -0500

    updated home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
+import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import ParkDetails from './pages/ParkDetails';
-import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import ParkDetails from './pages/ParkDetails'
+import Navbar from './components/Navbar'
 import ParkForms from './components/ParkForms'
 
 function App() {
   return (
     <div>
-
       <Navbar />
-      <ParkForms />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/parkdetails/:id' element={<ParkDetails />} />
-          <Route path='/create-park' element={<ParkForms />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/parkdetails/:id" element={<ParkDetails />} />
+          <Route path="/create-park" element={<ParkForms />} />
         </Routes>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/ParkForms.jsx
+++ client/src/components/ParkForms.jsx
@@ -1,10 +1,68 @@
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-const Forms = () => {
+const ParkForms = () => {
+  let navigate = useNavigate()
+
+  const initialState = {
+    name: '',
+    location: '',
+    picture_url: ''
+
+  }
+  const [formState, setFormState] = useState(initialState)
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    await axios.post('/api/create-park', formState)
+    setFormState(initialState)
+    await navigate('/')
+  }
+  function ValidButton() {
+    if (formState.name) {
+      return (
+        <button type="submit" className="validButton">
+          Create New Park
+        </button>
+      )
+    } else {
+      return <h3>Park name is required</h3>
+    }
+  }
   return (
     <div>
-        <Link>Add Theme Park</Link>
+      <form onSubmit={handleSubmit} className="form">
+        <h1 >Create Park</h1>
+        <input
+          placeholder="Name"
+          id="name"
+          type="text"
+          onChange={handleChange}
+          value={formState.name}
+        />
+        <input
+          placeholder="Location"
+          id="location"
+          type="text"
+          onChange={handleChange}
+          value={formState.name}
+        />
+        <input
+          placeholder="Picture URL:"
+          id="picture_url"
+          type="text"
+          onChange={handleChange}
+          value={formState.name}
+        />
+
+        <ValidButton />
+      </form>
     </div>
   )
 }
-export default Forms
+export default ParkForms
