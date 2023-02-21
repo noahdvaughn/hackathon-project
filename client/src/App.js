877b200afdd620989ff5205ab7072f2c00fa2cd6
Dominic Fuentes <militaryforce1@yahoo.com>
2023-02-21 16:01:39 -0600

    usestate inside app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Home from './pages/Home'
 import ParkDetails from './pages/ParkDetails'
 import Navbar from './components/Navbar'
 import ParkForms from './components/ParkForms'
+import axios from 'axios'
 
-function App() {
+function App(props) {
+
+    const [allParks, setAllParks] = useState([])
+  
+    const getAllParks = async () => {
+      try {
+        let res = await axios.get('/api/all-parks')
+        setAllParks(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }  
+
+    useEffect(() => {
+      getAllParks()
+    }, [props])
+
   return (
     <div>
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home getAllParks={getAllParks}/>} />
           <Route path="/parkdetails/:id" element={<ParkDetails />} />
           <Route path="/create-park" element={<ParkForms />} />
         </Routes>
@@ -19,5 +37,6 @@ function App() {
     </div>
   )
 }
+
 
 export default App
