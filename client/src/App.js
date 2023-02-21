2f715b6fdbc8f0ed9acac38a9e51b1a71d2dc6f4
jcurti2 <joshua.curtis24@gmail.com>
2023-02-21 12:33:43 -0800

    park details, delete button and function

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ParkDetails from './pages/ParkDetails';
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div>
-      
+     
     </div>
   );
 }
