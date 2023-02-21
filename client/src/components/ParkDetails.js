673bdfbfccf41a8de8e6f3018f96da7a80ce5a0c
jcurti2 <joshua.curtis24@gmail.com>
2023-02-21 11:28:52 -0800

    parkDetails component

---
+++ client/src/components/ParkDetails.js
@@ -0,0 +1,24 @@
+import { useState, useEffect} from 'react'
+import Home from './Home'
+import axios from 'axios'
+
+const ParkDetails = () => {
+    const [rides, setRides] = useState([])
+
+    // getPark();
+  return (
+    <div>
+       {
+//     rides && (rides.map((oneRide) => (
+//         <div key={oneRide._id} className='card'>
+//             <p>{oneRide.name}</p>
+//             <p>{oneRide.runTime}</p>
+//             <button className='delete' id='deleteRide' onClick={() => deleteRide(oneRide)}>Delete Ride</button>
+//         </div>
+//     )))
+        }
+    </div>
+  )
+}
+
+export default ParkDetails
