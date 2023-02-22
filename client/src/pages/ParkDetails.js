e805bde5b7f8cf67e6b82bf489fc2e76706c3c2f
jcurti2 <joshua.curtis24@gmail.com>
2023-02-22 07:43:51 -0800

    broken changes

--- client/src/pages/ParkDetails.js
+++ client/src/pages/ParkDetails.js
@@ -7,51 +7,67 @@ import { useLocation } from 'react-router-dom'
 const ParkDetails = ( ) => {
 
   const location = useLocation()
+
   const { park } = location.state
+
   console.log(park);
-      // const [rides, setRides] = useState('')
+
+      const [rides, setRides] = useState()
+
       // const [park, setPark] = useState('')
+
       const [updatePark, setUpdatePark] = useState([])
+
       const [newRide, setNewRide] = useState({
         name:'', runtime:'', park_id:park._id
       })
+
 //when able to pass props will pass park into parkDetails
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     await axios.post(`/api/create-ride/${park._id}`, newRide)
-    console.log(park);
-    await axios.get(`/api/get-ride-by-park-id/${park._id}`)
-  }
+    // console.log(park);
+    }
+
   const handleChange = (event) => {
     setNewRide(
       {...newRide, [event.target.name]: event.target.value}
     )
   }
+
+  const getRides = async () =>{
+    let res = await axios.get(`/api/get-ride-by-park-id/${park._id}`)
+    setRides(res)
+  }
+
+
   const deletePark = async () => {
           await axios.delete(`/delete-park/${park._id}`)
        }
 
-  const getOnePark = async () => {
-         const response = await axios.get(`/api/get-park/${park._id}`)
-          // setPark(response.data.park)
-          console.log(response);
-  }
+  // const getOnePark = async () => {
+  //        const response = await axios.get(`/api/get-park/${park._id}`)
+  //         // setPark(response.data.park)
+  //         console.log(response);
+  // }
 
   const deleteRide = async () => {
   //     await axios.delete(needs a new route if we use this function)
   //     getAllParks();
   }
 
-  useEffect(() =>{
-    getOnePark()
+  useEffect( () =>{
+    // getOnePark()
+    getRides()
+    
   },[])
-         
+  
   return (
     <div>
       <div>
-      {park.ride &&      
-      park.ride.map((oneRide) => (
+      {rides.data.ride &&      
+      rides.data.ride.map((oneRide) => (
                 <div key={oneRide._id} className='card'>
                     <p>{oneRide.name}</p>
                     <p>{oneRide.runTime}</p>
