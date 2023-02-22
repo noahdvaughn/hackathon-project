f9fb9f6b630c2c93dd11a490d414b6ee3c22ca9c
jcurti2 <joshua.curtis24@gmail.com>
2023-02-21 16:29:18 -0800

    successfully mapped over rides within park and rendered them

--- client/src/pages/ParkDetails.js
+++ client/src/pages/ParkDetails.js
@@ -10,11 +10,18 @@ const ParkDetails = () => {
   const deletePark = async () => {
           await axios.delete(`/delete-park/${park._id}`)
        }
+
   const getOnePark = async () => {
          const response = await axios.get(`/api/get-park/63f52e617a6e913b979cee84`)
           setPark(response.data.park)
           console.log(response);
   }
+
+  const deleteRide = async () => {
+  //     await axios.delete(needs a new route if we use this function)
+  //     getAllParks();
+  }
+
   useEffect(() =>{
     getOnePark()
   },[])
@@ -23,11 +30,11 @@ const ParkDetails = () => {
     <div>
       <div>
       {park &&      
-      park.rides.map((oneRide) => (
+      park.ride.map((oneRide) => (
                 <div key={oneRide._id} className='card'>
                     <p>{oneRide.name}</p>
                     <p>{oneRide.runTime}</p>
-                    {/* <button className='delete' id='deleteRide' onClick={() => deleteRide(oneRide)}>Delete Ride</button> */}
+                    <button className='delete' id='deleteRide' onClick={() => deleteRide(oneRide)}>Delete Ride</button>
                 </div>
             ))}
       </div>
