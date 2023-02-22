5541b9a75edc6f337703293cd54b9859687028c9
Dominic Fuentes <militaryforce1@yahoo.com>
2023-02-21 18:47:19 -0600

    move usestate from app.js to home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const Home = ({getAllParks}) => {
+const Home = () => {
+
+    const [allParks, setAllParks] = useState([])
+  
+    const getAllParks = async () => {
+      try {
+        let res = await axios.get('/api/all-parks')
+        console.log(res)
+        setAllParks(res.data.parks)
+      } catch (err) {
+        console.log(err)
+      }
+    }  
+
+    useEffect(() => {
+      getAllParks()
+    }, [])
+
   return (
     <div>
-      All Parks
+      {allParks && allParks.map((park) => (
+        <div>
+          <Link to={`/api/parkdetails/${park._id}`}>Parks</Link>
+        </div>
+      ))}
     </div>
   )
 }
